a0e7679d5539336fcf8285f4b2e62622c526d207
Yasir Khan <yasir.khan@zoopla.co.uk>
2021-05-19 16:51:10 +0100

    refactor into functions

--- src/import_from_csv.py
+++ src/import_from_csv.py
@@ -6,23 +6,26 @@ master_file = "src/files/main_offline_validation.xlsx"
 master_file_csv = "src/files/main_offline_validation.csv"
 gdpr_file_csv = "src/files/AMA-UK-–-WASAYA-PERSONAL-DATA-CONSENT-FORM-FOR-AMJ-INTERNATIONAL-1620046642.csv"
 
-con = sqlite3.connect("members_data.db")
-cur = con.cursor()
-cur.execute(
-    "CREATE TABLE IF NOT EXISTS  members (id, name, branch)"
+
+connection = sqlite3.connect("members_data.db")
+cursor = connection.cursor()
+cursor.execute(
+    "CREATE TABLE IF NOT EXISTS  members(id, name, branch)"
 )
 
 
-def inert_data():
+def insert_data():
     with open(master_file_csv, newline="") as csvfile:
         reader = csv.reader(csvfile)
         header = next(reader)
-        cur = con.cursor()
-        cur.executemany(
-            "INSERT INTO members VALUES (?, ?, ?)",
+        cursor = connection.cursor()
+        cursor.executemany(
+            "INSERT OR IGNORE INTO members VALUES (?, ?, ?)",
             ((r[0], r[1], r[5]) for r in reader),  # id, name, branch
         )
-        con.commit()
+
+
+insert_data()
 
 
 def output_non_matches():
@@ -32,14 +35,14 @@ def output_non_matches():
     output.writeheader()
     with open(gdpr_file_csv, newline="") as csvfile:
         reader = csv.reader(csvfile)
-        cur = con.cursor()
+        cursor = connection.cursor()
         for row in reader:
             id, gdpr_name = row[0], row[2]
-            cur.execute(
+            cursor.execute(
                 "SELECT name, branch FROM members WHERE id = ? AND name <> ?",
                 (id, gdpr_name),
             )
-            result = cur.fetchone()
+            result = cursor.fetchone()
             if result is not None:
                 output.writerow(
                     {
